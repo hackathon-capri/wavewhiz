e9eb8e8551695956ca62a93a3b836bda7fd21c09
anapauladominoni <anapauladom29@gmail.com>
2025-08-29 15:12:44 -0300

    Início da página de salvos

---
+++ projeto/src/components/SalvosComponent.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+
+</script>
+<template>
+  <div class="square-border"></div>
+  <div class="encontre suas lojas">
+ <h3>Encontre as suas lojas FAVORITAS</h3>
+ </div>
+ <div class="lojasCima">
+  <ul>
+    <li>
+      <p>Loja 1</p>
+      <span class="fa-solid fa-bookmark"></span>
+    </li>
+    <li>
+      <p>Loja 2</p>
+      <span class="fa-solid fa-bookmark"></span>
+    </li>
+    <li>
+      <p>Loja 3</p>
+      <span class="fa-solid fa-bookmark"></span>
+    </li>
+  </ul>
+ </div>
+ <div class="lojasBaixo">
+  <ul>
+    <li>
+      <p>Loja 4</p>
+      <span class="fa-solid fa-bookmark"></span>
+    </li>
+    <li>
+      <p>Loja 5</p>
+      <span class="fa-solid fa-bookmark"></span>
+    </li>
+    <li>
+      <p>Loja 6</p>
+      <span class="fa-solid fa-bookmark"></span>
+    </li>
+    </ul>
+ </div>
+</template>
