9d319c4afb3b2c32927a77b8bdd88d8608f7da67
emilly015 <emilly.v.duarte01@gmail.com>
2025-08-29 16:54:11 -0300

    Adicionando bandeiras de cartão #10

--- projeto/src/views/PagamentoView.vue
+++ projeto/src/views/PagamentoView.vue
@@ -2,18 +2,41 @@
 </script>
 <template>
   <main>
-    <ul>
+    <div class="cartoes">
+       <ul>
       <li>
-        <img src="/public/caixa-removebg-preview 1.png" alt="caixa">
+        <img src="/caixa-removebg-preview 1.png" alt="caixa-cartao">
+      </li>
+      <li class="elo">
+        <img src="/Elo-removebg-preview 1.png" alt="elo-cartao">
+      </li>
+      <li class="visa">
+        <img src="/Visa-removebg-preview 1.png" alt="visa-cartao">
       </li>
       <li>
-        <img src="/public/Elo-removebg-preview 1.png" alt="elo">
-      </li>
-      <li>
-        <img src="/public/Visa-removebg-preview 1.png" alt="visa">
-      </li>
-      <li>
+        <img src="/Mastercard-removebg-preview 1.png" alt="mastercard-cartao">
       </li>
     </ul>
+    </div>
   </main>
 </template>
+<style scoped>
+main{
+  background-color: #F5F5F5;
+}
+.cartoes ul{
+  display: flex;
+  list-style: none;
+}
+li{
+  background: #c7c3c3;
+  margin: 0 1vw 0 0;
+  width: 100px;
+  height: 60px;
+}
+img{
+  max-width: 80%;
+  max-height: 80%;
+}
+
+</style>
