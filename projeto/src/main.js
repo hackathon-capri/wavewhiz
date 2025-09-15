5340eb0faee6cb385303a8d50c24c92e1805fbfa
pietralavina <pietragiovana2008@gmail.com>
2025-09-15 16:45:30 -0300

    Página do Perfil #16

--- projeto/src/main.js
+++ projeto/src/main.js
@@ -13,28 +13,31 @@ const pinia = createPinia()
 router.afterEach((to) => {
   if (to.path === '/') {
     document.body.style.backgroundImage = "url('/teste1.png')"
-    document.body.style.backgroundRepeat = "no-repeat"
-    document.body.style.backgroundSize = "cover"
-    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundRepeat = 'no-repeat'
+    document.body.style.backgroundSize = 'cover'
+    document.body.style.backgroundPosition = 'center'
   } else if (to.path === '/cadastro') {
     document.body.style.backgroundImage = "url('/alot-shark.jpg')"
-    document.body.style.backgroundRepeat = "no-repeat"
-    document.body.style.backgroundSize = "cover"
-    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundRepeat = 'no-repeat'
+    document.body.style.backgroundSize = 'cover'
+    document.body.style.backgroundPosition = 'center'
   } else if (to.path === '/login') {
     document.body.style.backgroundImage = "url('/alot-shark.jpg')"
-    document.body.style.backgroundRepeat = "no-repeat"
-    document.body.style.backgroundSize = "cover"
-    document.body.style.backgroundPosition = "center"
+    document.body.style.backgroundRepeat = 'no-repeat'
+    document.body.style.backgroundSize = 'cover'
+    document.body.style.backgroundPosition = 'center'
   } else if (to.path === '/sobre-nos') {
-    document.body.style.backgroundImage = "linear-gradient(to bottom, #0a6b85, #002c38), url('/alot-shark.jpg')";
-  document.body.style.backgroundSize = "cover";
-  document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundImage =
+      "linear-gradient(to bottom, #0a6b85, #002c38), url('/alot-shark.jpg')"
+    document.body.style.backgroundSize = 'cover'
+    document.body.style.backgroundPosition = 'center'
+  } else if (to.path === '/perfil-empreendedor') {
+    document.body.style.backgroundColor = '#04394a'
+    document.body.style.backgroundImage = ''
   } else {
-    document.body.style.backgroundImage = ""
+    document.body.style.backgroundImage = ''
   }
 })
-
 
 app.use(pinia)
 app.use(router)
--- projeto/src/views/PerfilEmpreendedorView.vue
+++ projeto/src/views/PerfilEmpreendedorView.vue
@@ -15,25 +15,156 @@ onMounted(async () => {
       <img src="/public/login-icon.png" alt="Ícone de login" />
       <h1>Seu Perfil</h1>
     </div>
-
-    <div class="dados" v-if="perfil.nome_loja">
-      <div class="coluna">
-        <p><strong>Nome da Loja:</strong> {{ perfil.nome_loja }}</p>
-        <p><strong>Endereço:</strong> {{ perfil.endereco }}</p>
-        <p><strong>CPF/CNPJ:</strong> {{ perfil.cpf_cnpj }}</p>
-        <p><strong>Categoria:</strong> {{ perfil.categoria }}</p>
+    <div class="listas">
+      <div>
+        <ul>
+          <li>
+            <p>Nome da Loja:</p>
+          </li>
+          <li>
+            <p>Endereço:</p>
+          </li>
+          <li>
+            <p>CPF/CNPJ:</p>
+          </li>
+          <li>
+            <p>Categoria:</p>
+          </li>
+        </ul>
       </div>
-      <div class="coluna">
-        <p><strong>Nome de Usuário:</strong> {{ perfil.nome_usuario }}</p>
-        <p><strong>Data de Nascimento:</strong> {{ perfil.data_nascimento }}</p>
-        <p><strong>Telefone:</strong> {{ perfil.telefone }}</p>
-        <p><strong>E-mail:</strong> {{ perfil.email }}</p>
+      <div>
+        <ul>
+          <li>
+            <p>Nome de Usuário:</p>
+          </li>
+          <li>
+            <p>Data de Nascimento:</p>
+          </li>
+          <li>
+            <p>Telefone:</p>
+          </li>
+          <li>
+            <p>E-mail:</p>
+          </li>
+        </ul>
       </div>
     </div>
-
     <div class="botoes">
       <button>Editar Dados</button>
       <button>Adicionar itens</button>
     </div>
   </div>
 </template>
+
+<style scoped>
+.quadrado {
+  margin: 5vw auto;
+  width: 1300px;
+  min-height: 600px;
+  border: 5px solid white;
+  padding: 20px;
+  box-sizing: border-box;
+  color: white;
+  font-weight: bold;
+}
+.detalhes {
+  display: flex;
+  align-items: center;
+  margin: 20px 20px 20px 20px;
+}
+.detalhes img {
+  height: 70px;
+  width: 75px;
+}
+.detalhes h1 {
+  font-size: 1.4rem;
+  background-color: #08607C;
+  padding: 10px 30px 10px 30px;
+  margin-left: 15px;
+  border-radius: 100px;
+}
+.listas {
+  display: flex;
+  margin-top: 7vw;
+  font-size: 1.4rem;
+}
+.listas ul {
+  list-style: none;
+  margin: 0 10vw 0 10vw;
+}
+.listas li {
+  margin-bottom: 20px;
+}
+.botoes {
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+  margin-top: 2rem;
+  width: 100%;
+}
+
+.botoes button {
+  background-color: #08607C;
+  color: white;
+  border: none;
+  padding: 15px 35px;
+  font-weight: bold;
+  border-radius: 100px;
+  cursor: pointer;
+  font-size: 1.2rem;
+}
+
+.botoes button:hover {
+  background-color: #0e98c2;
+  transition: 0.3s;
+}
+
+/*---------->RESPONSIVIDADE<----------*/
+@media (max-width: 768px) {
+  .quadrado {
+    width: 95%;
+    padding: 15px;
+    min-height: auto;
+  }
+
+  .detalhes {
+    align-items: flex-start;
+    display: flex;
+    align-items: center;
+    margin: 20px 20px 20px 20px;
+  }
+
+  .detalhes img {
+    height: 50px;
+    width: 55px;
+    margin: 0 8px 2px 0;
+  }
+
+  .detalhes h1 {
+    font-size: 1.2rem;
+    padding: 8px 20px;
+    margin-left: 15px;
+  }
+
+  .listas {
+    flex-direction: column;
+    margin-top: 3rem;
+    font-size: 1rem;
+  }
+
+  .listas ul {
+    margin: 0 0 2rem 0;
+  }
+
+  .botoes {
+    flex-direction: column;
+    gap: 0.8rem;
+  }
+
+  .botoes button {
+    width: 100%;
+    padding: 12px 20px;
+    font-size: 1rem;
+  }
+}
+</style>
