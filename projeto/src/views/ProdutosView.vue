81872e2d415f41c1735ed23fd50753009d759a86
emilly015 <emilly.v.duarte01@gmail.com>
2025-09-17 17:05:01 -0300

    Inicio Página produtos #23

---
+++ projeto/src/views/ProdutosView.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+<template>
+  <main>
+    <div class="imagens">
+      <ul>
+        <li><img src="/public/image-removebg-preview(1) 1.png" alt="provisoria"></li>
+        <li><img src="/public/Rectangle 378.png" alt="provisoria2"></li>
+        <li class="maior"><img src="/public/Rectangle_376-removebg-preview 1.png" alt="provisoria3"></li>
+      </ul>
+    </div>
+    <div>
+      <h1>Andressa Urach - Borderline 3 versões</h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <p>
+        Conheça a trajetória intensa e transformadora de Andressa Urach em três fases marcantes: da fama à fé, da dor à superação. Um relato forte e real sobre saúde mental, autoconhecimento e renascimento.  Ideal para quem busca inspiração, reflexão e coragem para recomeçar
+      </p>
+      <p>R$</p>
+    </div>
+    <button>Adiconar ao carrinho</button>
+  </main>
+</template>
+<style scoped>
+</style>
